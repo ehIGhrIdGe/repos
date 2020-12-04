6662e09cf2f4d864bf72af36f583292d3e9cc56f
ehIGhrIdGe <rzlr4hiqvexgw88n@outlook.jp>
2020-12-04 11:29:46 +0900

    Square.cs First commit

---
+++ Csharp_11/Square/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square();
+            Console.WriteLine(square.Width);
+            Console.WriteLine(square.CalculationArea());
+
+            Console.WriteLine();
+            square = new Square(4);
+            Console.WriteLine(square.Width);
+            Console.WriteLine(square.CalculationArea());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Csharp_11/Square/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square
+{
+    class Square
+    {
+        public double Width { get; set; }
+
+        public Square()
+        {
+            Width = 0;
+        }
+
+        public Square(double width)
+        {
+            Width = width;
+        }
+
+        public double CalculationArea()
+        {
+            var result = Width * Width;
+
+            return result;
+        }
+
+    }
+}
