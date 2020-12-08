cd222a35e1d9dae532daaff40eaac9d7ca8a4cd7
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2020-12-08 17:26:26 +0900

    CSharp_16/TestInterface/IName.cs
    added IName interface

---
+++ CSharp_16/TestInterface/Shape/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestInterface
+{
+    class Circle : Shape
+    {
+        public override string Name { get { return this.GetType().Name; } }
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+            Radius = 0;
+        }
+
+        public Circle(double radisu)
+        {
+            Radius = radisu;
+        }
+
+        public override double CalculateArea()
+        {
+            return Radius * Radius * 3.14;
+        }
+    }
+}
---
+++ CSharp_16/TestInterface/Shape/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestInterface
+{
+    abstract class Shape : IName
+    {
+        public abstract string Name { get; }
+
+        public abstract double CalculateArea();        
+
+        public override string ToString()
+        {
+            return $"[{Name}] : {CalculateArea()}";
+        }
+    }
+}
