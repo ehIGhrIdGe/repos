60d89e1046396776d4e903db02fc807f01261b4b
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2020-12-09 16:19:18 +0900

    Csharp_17/SmpleException/SmpleException.sln
    first commit

---
+++ Csharp_17/SmpleException/GetFile/GetFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SmpleException.GetFile
+{
+    class GetFile
+    {
+        private string FilePath { get; set; }
+
+        public GetFile()
+        {
+            FilePath = Console.ReadLine();
+        }
+
+        public void GetFileContents()
+        {
+            try
+            {
+                using (var reader = new StreamReader(FilePath))
+                {
+                    var text = reader.ReadToEnd();
+
+                    Console.WriteLine($"\n↓↓↓----ファイルの内容です----↓↓↓\n{text}");
+                }
+            }
+            catch(IOException e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Csharp_17/SmpleException/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace SmpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("↓↓↓----ファイルのパスを入力----↓↓↓");
+                var s = new GetFile.GetFile();
+                s.GetFileContents();
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine($"[e.Message] => {e.Message}");
+                Console.WriteLine($"[e.InnerException] => {e.InnerException}");
+                Console.WriteLine($"[e.StackTrace] => {e.StackTrace}");
+                Console.WriteLine($"[e.ToString] => {e.ToString()}");
+            }
+            
+            
+        }
+    }
+}
