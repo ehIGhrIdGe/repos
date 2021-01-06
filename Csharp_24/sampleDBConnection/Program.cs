ee322147a4f2c46c4a00dbc82468637f3eb215cc
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2021-01-06 16:56:53 +0900

    sampleDBConnection/Program.cs
    end
    
    add commit

--- Csharp_24/sampleDBConnection/Program.cs
+++ Csharp_24/sampleDBConnection/Program.cs
@@ -14,13 +14,13 @@ namespace sampleDBConnection
             csBuilder.InitialCatalog = "testdb";
             csBuilder.IntegratedSecurity = true;
 
-            using (SqlConnection sqlConnection = new SqlConnection(csBuilder.ConnectionString))
+            try
             {
-                try
+                using (SqlConnection sqlConnection = new SqlConnection(csBuilder.ConnectionString))
                 {
                     sqlConnection.Open();
 
-                    if(sqlConnection.State == ConnectionState.Open)
+                    if (sqlConnection.State == ConnectionState.Open)
                     {
                         Console.WriteLine("DB Connection is open.");
                     }
@@ -40,17 +40,96 @@ namespace sampleDBConnection
                         Console.WriteLine("DB Connection is close.");
                     }
                 }
-                catch(SqlException e)
+
+                //Selectコマンドを実行してみる
+                Console.WriteLine();
+                using (SqlConnection sqlConnection = new SqlConnection(csBuilder.ConnectionString))
                 {
-                    Console.WriteLine(e.GetType().Name.ToString());
-                    Console.WriteLine(e.Message);
-                    Console.WriteLine(e.ToString());
+                    sqlConnection.Open();
+
+                    using (var cmd = new SqlCommand())
+                    {
+                        cmd.Connection = sqlConnection;
+                        cmd.CommandText = "SELECT * FROM Employee;";
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Console.WriteLine("適当に全件表示");
+                        while (reader.Read())
+                        {
+                            for (var i = 0; i < reader.FieldCount; i++)
+                            {
+                                Console.Write($"{reader[i].ToString().Trim()}-");
+                            }
+                            Console.WriteLine();
+                        }
+                        reader.Close();
+                        reader = cmd.ExecuteReader();
+
+                        Console.WriteLine("\n社員番号、氏名をハイフン区切りで表示。取得件数も表示。");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]}-{reader[1]}");
+                        }
+                        reader.Close();
+                        cmd.CommandText = "SELECT COUNT(*) FROM Employee;";
+                        Console.WriteLine($"取得件数は{cmd.ExecuteScalar()}件です。");
+                    }
+                    sqlConnection.Close();
+
+                    Console.ReadKey();
                 }
-                
 
+                //Update,delete等を実行してみる
+                Console.WriteLine();
+                using (SqlConnection sqlConnection = new SqlConnection(csBuilder.ConnectionString))
+                {
+                    sqlConnection.Open();
+
+                    using (var cmd = new SqlCommand())
+                    {
+                        cmd.Connection = sqlConnection;
+                        cmd.CommandText = "DELETE FROM Employee;";
+                        Console.WriteLine($"削除件数は{cmd.ExecuteNonQuery()}件です。");
+                        Console.ReadKey();
+
+                        cmd.CommandText = "insert into [testdb].[dbo].[Employee] select * from testdb.dbo.社員;";
+                        Console.WriteLine($"追加件数は{cmd.ExecuteNonQuery()}件です。");
+                        Console.ReadKey();
+
+                        cmd.CommandText = "SELECT * FROM Employee;";
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        Console.WriteLine("\n更新前の状態です。");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]}-{reader[1].ToString().Trim()}-{reader[2]}");
+                        }
+                        reader.Close();
+                        Console.ReadKey();
+
+                        cmd.CommandText = "UPDATE [testdb].[dbo].[Employee] SET 給与 = 給与*10;";
+                        Console.WriteLine($"\n更新件数は{cmd.ExecuteNonQuery()}件です。");
+                        Console.ReadKey();
+
+                        cmd.CommandText = "SELECT * FROM Employee;";
+                        reader = cmd.ExecuteReader();
+                        Console.WriteLine("\n更新後の状態です。");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]}-{reader[1].ToString().Trim()}-{reader[2]}");
+                        }
+                        reader.Close();
+                        Console.ReadKey();
+                    }                                           
+                    sqlConnection.Close();
+                }
             }
-
-            Console.WriteLine("Hello World!");
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.GetType().Name.ToString());
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
