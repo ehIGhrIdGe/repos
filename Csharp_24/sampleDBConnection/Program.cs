6a7c9b3c3ced8c0fb5eadf0c0c4132bbd2fd7da8
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2021-01-06 14:20:53 +0900

    SqlConnection
    add commit

---
+++ Csharp_24/sampleDBConnection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace sampleDBConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=DESKTOP-LO22HNR;Initial Catalog=testdb;Integrated Security=True"))
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    if(sqlConnection.State == ConnectionState.Open)
+                    {
+                        Console.WriteLine("DB Connection is open.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("DB Connection is not open.");
+                    }
+
+                    sqlConnection.Close();
+
+                    if (sqlConnection.State == ConnectionState.Open)
+                    {
+                        Console.WriteLine("DB Connection is open even.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("DB Connection is close.");
+                    }
+                }
+                catch(SqlException e)
+                {
+                    Console.WriteLine(e.GetType().Name.ToString());
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.ToString());
+                }
+                
+
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
