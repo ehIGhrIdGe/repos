ff87e46965a6040c5e4c2dae06494f9d0dea0541
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2021-01-06 17:07:48 +0900

    DataTableSample.sln
    start
    
    add commit

---
+++ Csharp_24/DataTableSample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataTableSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var csBuilder = new SqlConnectionStringBuilder();
+                csBuilder.DataSource = "DESKTOP-LO22HNR";
+                csBuilder.InitialCatalog = "testdb";
+                csBuilder.IntegratedSecurity = true;
+
+                using (var sqlConnection = new SqlConnection(csBuilder.ConnectionString))
+                {
+                    sqlConnection.Open();
+
+                    using (var cmd = new SqlCommand())
+                    {
+                        cmd.Connection = sqlConnection;
+                        cmd.CommandText = "SELECT * FROM Employee;";
+
+                        //ExecuteReader()での結果をまず確認
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            
+                        }
+
+                        using (var ada = new SqlDataAdapter())
+                        {
+                            ada.SelectCommand = cmd;
+
+                            using (var dt = new DataTable())
+                            {
+                                ada.Fill(dt);
+                                sqlConnection.Close();
+
+                                Console.WriteLine($"行数は{dt.Rows.Count}です。");
+                                Console.WriteLine($"列数は{dt.Columns.Count}です。");
+                                Console.WriteLine($"1行目第2項目は{dt.Rows[0][1].ToString().Trim()}です。");
+                            }
+                        }
+                    }
+                }
+
+                Console.ReadKey();
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.GetType().Name.ToString());
+                Console.WriteLine(e.Server.ToString());
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine(e.Errors.ToString());
+                Console.WriteLine(e.Source.ToString());
+            }
+        }
+    }
+}
