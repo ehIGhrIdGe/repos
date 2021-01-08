b4c0f678e257660c0111c956e04d5efaeb4486d7
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2021-01-08 18:12:01 +0900

    DataGridViewSample.sln
    Form1 edited
    
    add commit

---
+++ Csharp_26/DataGridViewSample/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace DataGridViewSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEdit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.colDelete = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col1,
+            this.Col2,
+            this.colEdit,
+            this.colDelete});
+            this.dataGridView1.Location = new System.Drawing.Point(51, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 20;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(706, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Col1
+            // 
+            this.Col1.DataPropertyName = "社員番号";
+            this.Col1.HeaderText = "社員番号";
+            this.Col1.MinimumWidth = 8;
+            this.Col1.Name = "Col1";
+            this.Col1.ReadOnly = true;
+            this.Col1.Width = 150;
+            // 
+            // Col2
+            // 
+            this.Col2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Col2.DataPropertyName = "氏名";
+            this.Col2.HeaderText = "氏名";
+            this.Col2.MinimumWidth = 8;
+            this.Col2.Name = "Col2";
+            this.Col2.ReadOnly = true;
+            // 
+            // colEdit
+            // 
+            this.colEdit.HeaderText = "";
+            this.colEdit.MinimumWidth = 8;
+            this.colEdit.Name = "colEdit";
+            this.colEdit.ReadOnly = true;
+            this.colEdit.Text = "編集";
+            this.colEdit.UseColumnTextForButtonValue = true;
+            this.colEdit.Width = 50;
+            // 
+            // colDelete
+            // 
+            this.colDelete.HeaderText = "";
+            this.colDelete.MinimumWidth = 8;
+            this.colDelete.Name = "colDelete";
+            this.colDelete.ReadOnly = true;
+            this.colDelete.Text = "削除";
+            this.colDelete.UseColumnTextForButtonValue = true;
+            this.colDelete.Width = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col2;
+        private System.Windows.Forms.DataGridViewButtonColumn colEdit;
+        private System.Windows.Forms.DataGridViewButtonColumn colDelete;
+    }
+}
+
---
+++ Csharp_26/DataGridViewSample/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using NLog;
+
+
+namespace DataGridViewSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var log = LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DataGridViewSample.Properties.Settings.接続文字列1"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter())
+                        {
+                            con.Open();
+
+                            cmd.Connection = con;
+                            var tra = con.BeginTransaction();
+                            try
+                            {
+                                using (DataTable dt = new DataTable())
+                                {
+                                    
+
+                                    cmd.CommandText = @"SELECT* FROM Employee";
+                                    cmd.Transaction = tra;
+                                    adapter.SelectCommand = cmd;
+
+                                    adapter.Fill(dt);
+
+                                    dataGridView1.AutoGenerateColumns = false;
+                                    dataGridView1.DataSource = dt;
+                                }
+                            }
+                            catch (SqlException)
+                            {
+                                tra.Rollback();
+                                log.Info("-------------------ロールバックしました。-------------------");
+                                throw;
+                            }
+                            catch (Exception)
+                            {
+                                throw;
+                            }                            
+                        }
+
+
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                log.Error($"GetType().Name => {ex.GetType().Name}");
+                log.Error($"Server         => {ex.Server}");
+                log.Error($"Message        => {ex.Message}");
+                log.Error($"Errors         => {ex.Errors}");
+                log.Error($"Source         => {ex.Source}");
+                log.Error($"InnerException => {ex.InnerException}");
+                log.Error($"StackTrace     => {ex.StackTrace}");
+            }
+            catch (Exception ex)
+            {
+                log.Error($"GetType().Name => {ex.GetType().Name}");
+                log.Error($"Message        => {ex.Message}");
+                log.Error($"StackTrace     => {ex.StackTrace}");
+                log.Error($"Source         => {ex.Source}");
+                log.Error($"InnerException => {ex.InnerException}");
+                log.Error($"TargetSite     => {ex.TargetSite}");
+            }
+        }
+    }
+}
