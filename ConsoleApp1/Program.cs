3b74f0de9c55bb7076aa01525fda79c991949733
ehIGhrIdGe <rZlR4hiQVEXGw88n@outlook.jp>
2021-04-06 09:33:56 +0900

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var datetime = string.Format(CultureInfo.CreateSpecificCulture("en-US"), "{0:dd-MMM-yyyy}", DateTime.Now);
+            Console.WriteLine("0:dd-MMM-yyyy==={0}",datetime);
+
+            datetime = string.Format("{0:dd-MMM-yyyy}", DateTime.Now);
+            Console.WriteLine("0:dd-MMM-yyyy==={0}", datetime);
+
+            datetime = string.Format(CultureInfo.CreateSpecificCulture("en-US"), "{0:dd-MMMM-yyyy}", DateTime.Now);
+            Console.WriteLine("0:dd-MMMM-yyyy==={0}", datetime);
+
+            datetime = string.Format("{0:dd-MMMM-yyyy}", DateTime.Now);
+            Console.WriteLine("0:dd-MMMM-yyyy==={0}", datetime);
+
+            datetime = string.Format("{0:dd-MM-yyyy}", DateTime.Now);
+            Console.WriteLine("0:dd-MM-yyyy==={0}", datetime);
+        }
+    }
+}
