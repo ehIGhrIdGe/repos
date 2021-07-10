f20c4ee667d99b9e72e95ebed6da5f636d94b52d
ehIGhrIdGe <rzlr4hiqvexgw88n@outlook.jp>
2021-07-10 11:29:37 +0900

    echat
    first commit

---
+++ eChat/eChat/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Msg = "Let's enjoy chat!";
+            return View();
+        }
+    }
+}
---
+++ eChat/eChat/Models/DbUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace eChat.Models
+{
+    public class DbUtil
+    {
+        public static string ConnectionStr { get; set; }
+    }
+}
