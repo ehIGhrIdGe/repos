6b3e890506ccd41e430f56eff7851a5b3743ea96
ehIGhrIdGe <rzlr4hiqvexgw88n@outlook.jp>
2021-07-10 15:06:02 +0900

    Create Models/ChatLogs.cs Models/ChatMessage.cs
    Edit
    Models/DbUtil.cs

--- eChat/eChat/Models/DbUtil.cs
+++ eChat/eChat/Models/DbUtil.cs
@@ -2,12 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace eChat.Models
 {
     public class DbUtil
     {
         public static string ConnectionStr { get; set; }
+
+        public static SqlConnection NewConnection()
+        {
+            SqlConnection con = null;
+            try
+            {
+                if (string.IsNullOrWhiteSpace(ConnectionStr))
+                {
+                    throw new ArgumentException();
+                }
+                con = new SqlConnection(ConnectionStr);
+                con.Open();
+                
+            }
+            catch
+            {
+                con?.Dispose();
+            }
+            return con;
+        }
+
+        public static IDisposable GetConnection(out SqlConnection connection)
+        {
+            connection = NewConnection();
+            return connection;
+        }
+
+        public void GetMessages()
+        {
+            SqlConnection conn;
+            using (GetConnection(out conn))
+            {
+                using (IDbTransaction trn = conn.BeginTransaction())
+                {
+                    try
+                    {
+                        var cmd = new SqlCommand("SELECT * FROM [ChatLogs]", conn);
+                        var ada = new SqlDataAdapter(cmd);
+                        var chatLogsDt = new DataTable();
+                        ada.Fill(chatLogsDt);
+                    }
+                    finally
+                    {
+                        if (trn.Connection != null)
+                        {
+                            trn.Rollback();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ eChat/eChat/eChat_CreateTable.sql
@@ -0,0 +1,28 @@
+﻿USE Echat
+ 
+CREATE TABLE [dbo].[ChatLogs](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[PostAt] [datetime2](7) NOT NULL,
+	[Message] [nvarchar](max) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[UserId] [nvarchar](50) NULL,
+ CONSTRAINT [PK_ChatLogs] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+ 
+CREATE TABLE [dbo].[Users](
+	[UserId] [nvarchar](50) NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[PasswordType] [tinyint] NOT NULL,
+	[PasswordSalt] [nvarchar](255) NULL,
+	[Password] [nvarchar](255) NULL,
+	[IsAdministrator] [bit] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Users] ADD  CONSTRAINT [DF_Users_IsAdministrator]  DEFAULT ((0)) FOR [IsAdministrator]
